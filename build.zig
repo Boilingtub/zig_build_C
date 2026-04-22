e2c7ec9ddd9bdaa3e4a0d92c75f92972b67f7a5a
Jan-Hendrik <jhbrink59@gmail.com>
2026-04-22 12:11:21 +0200

    Updated for zig 0.16 compatability

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
@@ -10,19 +9,19 @@ pub fn build(b: *std.Build) void {
         .root_module = std.Build.Module.create(b, .{
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
         }),
-        .linkage = .static,
+        .linkage = .dynamic,
     });
 
-    exe.root_module.addIncludePath(std.Build.path(b,"./include/"));
-    exe.addCSourceFiles(.{
+    exe.root_module.addIncludePath(std.Build.path(b,"include/"));
+    exe.root_module.addCSourceFiles(.{
         .root = b.path("."),
         .files = &[_][]const u8{"src/main.c"},
         .flags = &[_][]const u8 {"-std=c23","-Wall","-Wextra","-gen-cdb-fragment-path","cdb-frags"},
     });
-    exe.root_module.addLibraryPath(std.Build.path(b,"./lib/"));
-    exe.root_module.linkSystemLibrary("",.{});
-    exe.linkLibC();
+    exe.root_module.addLibraryPath(std.Build.path(b,"lib/"));
+    //exe.root_module.linkSystemLibrary("",.{});
 
     b.installArtifact(exe);
 
@@ -41,25 +40,29 @@ pub fn build(b: *std.Build) void {
 }
 
 fn collect_cdb_fragments(_: *std.Build.Step, _: std.Build.Step.MakeOptions) anyerror!void {
-    const outf = try std.fs.cwd().createFile("compile_commands.json", .{});
-    defer outf.close();
+    var threaded : std.Io.Threaded = .init_single_threaded;
+    const io = threaded.io();
+    defer threaded.deinit();
 
-    var dir = std.fs.cwd().openDir("cdb-frags", .{.iterate = true}) catch {
+    var outf = try std.Io.Dir.cwd().createFile(io, "compile_commands.json", .{});
+    defer outf.close(io);
+    var dir = std.Io.Dir.cwd().openDir(io, "cdb-frags", .{.iterate = true}) catch {
         std.debug.print("Failed to open ./cdb-frags/", .{});
         return;
     };
-    defer dir.close();
+    defer dir.close(io);
 
-    try outf.writeAll("[");
+    try outf.writeStreamingAll(io,"[");
     var iter = dir.iterate();
-    while(try iter.next()) |entry| {
+    while(try iter.next(io)) |entry| {
         const fpath = try std.fmt.allocPrint(
             std.heap.page_allocator, "cdb-frags/{s}", .{entry.name}
         );
-        const contents = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, fpath, 1024*1024);
-        try outf.seekFromEnd(0);
-        try outf.writeAll(contents);
+        const contents = try std.Io.Dir.cwd().readFileAlloc(io, fpath, std.heap.page_allocator, .limited(1024*1024));
+        var outf_w = outf.writer(io, contents);
+        try outf_w.seekTo(0);
+        try outf.writeStreamingAll(io, contents);
     }
-    try outf.writeAll("]");
+    try outf.writeStreamingAll(io, "]");
 
 }
